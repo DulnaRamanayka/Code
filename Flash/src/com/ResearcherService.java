70357b0a4177d0e8b6bc171a6b24259ab16da33d
AnduniPamoda <andunipamoda98@gmail.com>
2021-04-19 20:09:21 +0530

    create retrieve method and retrieve service methods

--- Flash/src/com/ResearcherService.java
+++ Flash/src/com/ResearcherService.java
@@ -2,6 +2,7 @@ package com;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -32,4 +33,12 @@ public class ResearcherService {
 		System.out.println(researcherName);
 		return output; 
 	}
+	
+	@GET
+	@Path("/") 
+	@Produces(MediaType.TEXT_HTML) 
+	public String readItems() 
+	 { 
+		return itemObj.readItems();  
+	 }
 }
