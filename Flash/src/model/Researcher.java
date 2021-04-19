ba167801b08d7d343b84c8ae54b62068a83d14e7
AnduniPamoda <andunipamoda98@gmail.com>
2021-04-19 19:36:09 +0530

    create database connection method

--- Flash/src/model/Researcher.java
+++ Flash/src/model/Researcher.java
@@ -1,5 +1,24 @@
 package model;
 
+import java.sql.*; 
+
 public class Researcher {
 
+	private Connection connect() 
+	 { 
+		Connection con = null; 
+		try
+		{ 
+			Class.forName("com.mysql.jdbc.Driver"); 
+	 
+			//Provide the correct details: DBServer/DBName, username, password 
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paflab6", "root", ""); 
+			System.out.println("Successfully connected");
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		} 
+		return con; 
+	 }
 }
